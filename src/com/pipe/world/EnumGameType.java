af45046fc3e1da02957f5c08d19559c933045c38
ShineLoliMC <thomasbar59@gmail.com>
2020-05-30 16:58:20 +0200

    Update EnumGameType.java

--- src/com/pipe/world/EnumGameType.java
+++ src/com/pipe/world/EnumGameType.java
@@ -1,25 +1,44 @@
 package com.pipe.world;
 
+import com.pipe.entity.PlayerCapabilities;
+
 public enum EnumGameType {
 
-    NOT_SET(-1),
-    SURVIVAL(0),
-    CREATIVE(1),
-    ADVENTURE(2),
-    SPECTATOR(3);
+    NOT_SET(-1, new PlayerCapabilities(0.1F,0F,false,false,false), "" , ""),
+    SURVIVAL(0, new PlayerCapabilities(0.1F,0.1F,false,false,false), "s", "survival"),
+    CREATIVE(1, new PlayerCapabilities(0.1F,0.1F,true,true,true), "c", "creative"),
+    ADVENTURE(2, new PlayerCapabilities(0.1F,0.1F,false,false,false), "a", "adventure"),
+    SPECTATOR(3, new PlayerCapabilities(0.1F,0.1F,true,true,false), "spec", "spectator");
 
     ///////////////////////////////////////////////////////////////////////////
 
     private int id;
+    private PlayerCapabilities pc;
+    private String shortname, fullname;
 
     ///////////////////////////////////////////////////////////////////////////
 
-    EnumGameType(int id) {
+    EnumGameType(int id, PlayerCapabilities pc, String shortname, String fullname) {
         this.id = id;
+        this.pc = pc;
+        this.fullname = fullname;
+        this.shortname = shortname;
     }
 
     ///////////////////////////////////////////////////////////////////////////
 
+    public PlayerCapabilities getPlayerCapabilities() {
+		return pc;
+	}
+    
+    public String getShortName() {
+		return shortname;
+	}
+    
+    public String getFullName() {
+		return fullname;
+	}
+    
     public int getId() {
         return id;
     }
